158db9674a29bab99cfdc7c338f94329a8730b88
Michael McNees <mcnees.michael@gmail.com>
2026-03-23 23:03:13 -0400

    feat: add deterministic tag-to-hue hash utility

---
+++ src/utils/tag-color.ts
@@ -0,0 +1,9 @@
+// src/utils/tag-color.ts
+export function hashToHue(str: string): number {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    hash = hash & hash;
+  }
+  return Math.abs(hash) % 360;
+}
